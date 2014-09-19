61bfc65db2d07b813dba9675a7740326995f71ce
Ville Viklund <ville.viklund@gmail.com>
2014-09-20 00:56:13 +0300

    Added POST, PUT and DELETE routes for exercises

--- index.php
+++ index.php
@@ -14,4 +14,35 @@ $app->get('/exercises', function() use ($app) {
     $app->response->body(json_encode($exercises));
 });
 
+/**
+ * POST request for exercises
+ */
+$app->post('/exercises', function() use ($app) {
+    $exercise = ORM::for_table('exercises')->create();
+    $exercise->name = $app->request->post('name');
+    $exercise->description = $app->request->post('description');
+
+    return $exercise->save();
+});
+
+/**
+ * PUT request for exercises
+ */
+$app->put('/exercises/:id', function($id) use ($app) {
+    $exercise = ORM::for_table('exercises')->findOne($id);
+    $exercise->name = $app->request->put('name');
+    $exercise->description = $app->request->put('description');
+
+    return $exercise->save();
+});
+
+/**
+ * DELETE request for exercises
+ */
+$app->delete('/exercises/:id', function($id) use ($app) {
+    $exercise = ORM::for_table('exercises')->findOne($id);
+
+    return $exercise->delete();
+});
+
 $app->run();
